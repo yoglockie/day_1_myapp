63a8193cdabc4e4572f9f889a2b0519c6568db0d
yoglockie <yogilokesh916@gmail.com>
2023-08-22 22:35:27 +0530

    m

--- src/components/imgCard.js
+++ src/components/imgCard.js
@@ -7,16 +7,7 @@ import data from '../data';
 const imgCard = () => {
   return (
     <div>
-       <Card style={{ width: '18rem' }}>
-    <Card.Img variant="top" src={data[0].imgUrl} />
-    <Card.Body>
-      <Card.Title>{data[0].title}</Card.Title>
-      <Card.Text>
-      {data[0].desc}
-      </Card.Text>
-      <Button variant="primary">Go somewhere</Button>
-    </Card.Body>
-  </Card>
+       Loki
     </div>
   )
 }
